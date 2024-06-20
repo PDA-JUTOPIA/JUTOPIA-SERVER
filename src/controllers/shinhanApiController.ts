bcb2770b65e5cebd1fda6949bf443f631c33b9c9
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-06-20 20:10:23 +0900

    feat: 신한투자증권 OpenAPI 중 투자전략 API에 대해 데이터 수집하고 DB에 저장하는 스케줄링 기능 구현

--- src/controllers/shinhanApiController.ts
+++ src/controllers/shinhanApiController.ts
@@ -7,7 +7,7 @@ import { sequelize } from "../models";
 import logger from "../logger";
 import moment from "moment-timezone";
 
-export interface MarketIssue {
+export interface ColumnList {
   bbsName: string;
   title: string;
   regDate: string;
@@ -30,33 +30,79 @@ if (!apiKeyValue) {
   throw new Error("API_KEY_VALUE is not defined in the environment variables.");
 }
 
+const fetchMarketIssues = async (): Promise<ColumnList[]> => {
+  const response = await instance.get(
+    "https://gapi.shinhansec.com:8443/openapi/v1.0/strategy/market-issue",
+    {
+      headers: {
+        apiKey: apiKeyValue,
+        "Access-Control-Allow-Origin": "*",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+      },
+    }
+  );
+  return response.data.dataBody.list.map((item: any) => ({
+    bbsName: item.bbsName,
+    title: item.title,
+    regDate: item.regDate,
+    messageId: item.messageId,
+    attachmentUrl: item.attachmentUrl,
+    content: item.content,
+    viewCount: item.viewCount,
+    writer: item.writer,
+  }));
+};
+
+const fetchInvestStrategies = async (): Promise<ColumnList[]> => {
+  const response = await instance.get(
+    "https://gapi.shinhansec.com:8443/openapi/v1.0/strategy/invest",
+    {
+      headers: {
+        apiKey: apiKeyValue,
+        "Access-Control-Allow-Origin": "*",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+      },
+    }
+  );
+  return response.data.dataBody.list.map((item: any) => ({
+    bbsName: item.bbs_name,
+    title: item.title,
+    regDate: item.reg_date,
+    messageId: item.message_id,
+    attachmentUrl: item.attachment_url,
+    content: item.content,
+    viewCount: item.view_count,
+    writer: item.writer,
+  }));
+};
+
 export const fetchAndStoreMarketIssues = async (): Promise<void> => {
   try {
     logger.info("Fetching market issues from Shinhan API");
-    const shinhanResponse = await instance.get(
-      "https://gapi.shinhansec.com:8443/openapi/v1.0/strategy/market-issue",
-      {
-        headers: {
-          apiKey: apiKeyValue,
-          "Access-Control-Allow-Origin": "*",
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
-        },
-      }
-    );
+    const marketIssues = await fetchMarketIssues();
+    logger.info(
+      `Fetched ${marketIssues.length} market issues from Shinhan API`
+    ); // API 호출 후 받아온 데이터 개수 로그
 
-    const issues: MarketIssue[] = shinhanResponse.data.dataBody.list;
-    logger.info(`Fetched ${issues.length} market issues from Shinhan API`); // API 호출 후 받아온 데이터 개수 로그
+    logger.info("Fetching investment strategies from Shinhan API");
+    const investStrategies = await fetchInvestStrategies();
+    logger.info(
+      `Fetched ${investStrategies.length} investment strategies from Shinhan API`
+    ); // API 호출 후 받아온 데이터 개수 로그
+
+    const allIssues = [...marketIssues, ...investStrategies];
 
     // DB에서 기존 데이터를 조회
     const existingIssues = await ColumnBoard.findAll({
       attributes: ["bbs_name", "title", "reg_date"],
       where: {
         title: {
-          [Op.in]: issues.map((issue) => issue.title),
+          [Op.in]: allIssues.map((issue) => issue.title),
         },
         reg_date: {
-          [Op.in]: issues.map((issue) => issue.regDate),
+          [Op.in]: allIssues.map((issue) => issue.regDate),
         },
       },
     });
@@ -66,13 +112,11 @@ export const fetchAndStoreMarketIssues = async (): Promise<void> => {
     ); // 기존 데이터베이스에서 조회된 데이터 개수 로그
 
     // 중복 여부 확인
-    const newIssues = issues.filter((issue) => {
+    const newIssues = allIssues.filter((issue) => {
       return !existingIssues.some((existingIssue) => {
         const existingDate = moment(existingIssue.reg_date).format(
           "YYYY.MM.DD"
         ); // 날짜 문자열 변환
-        // logger.info(`Comparing ${existingIssue.title} with ${issue.title}`); // 제목 비교 로그
-        // logger.info(`Comparing ${existingDate} with ${issue.regDate}`); // 날짜 비교 로그
         return (
           existingIssue.title === issue.title && existingDate === issue.regDate
         );
@@ -94,8 +138,8 @@ export const fetchAndStoreMarketIssues = async (): Promise<void> => {
         content: issue.content,
       });
       logger.info(
-        `Stored new issue: ${issue.title} ${issue.writer} ${issue.regDate}` // 삽입된 데이터 로그
-      );
+        `Stored new issue: ${issue.title} ${issue.writer} ${issue.regDate}`
+      ); // 삽입된 데이터 로그
     }
   } catch (error: unknown) {
     if (error instanceof Error) {
@@ -112,7 +156,7 @@ export const getMarketIssues = async (
   res: Response
 ): Promise<void> => {
   try {
-    logger.info("Manual trigger to fetch market issues"); // 수동 트리거 로그
+    logger.info("Manual trigger to fetch market issues");
     await fetchAndStoreMarketIssues();
     res.json({ message: "Data fetched and stored successfully" });
   } catch (error: unknown) {
