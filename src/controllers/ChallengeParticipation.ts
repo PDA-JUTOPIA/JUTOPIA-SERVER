5e6b0b3a80e74dfe420420f24a0d5b4f0c505c49
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-06-25 17:30:59 +0900

    feat: 이메일과 challenge_id를 통해 참여중인 challenge_participation_id를 조회

--- src/controllers/ChallengeParticipation.ts
+++ src/controllers/ChallengeParticipation.ts
@@ -28,3 +28,39 @@ export async function getChallengesByEmail(req: Request, res: Response) {
     res.status(500).json({ error: "Failed to fetch challenge IDs" });
   }
 }
+
+// 이메일과 challenge_id를 통해 참여중인 challenge_participation_id를 조회 => 배열로 리턴
+export async function getParticipationId(req: Request, res: Response) {
+  try {
+    const { email, challenge_id } = req.body;
+    if (typeof email !== "string" || typeof challenge_id !== "number") {
+      return res
+        .status(400)
+        .json({ message: "Invalid email or challenge_id parameter" });
+    }
+
+    const userId = await getUserIdByEmail(email);
+    console.log(`${userId}  ${email}   ${challenge_id}`);
+    if (!userId) {
+      console.log;
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const participations = await ChallengeParticipation.findAll({
+      where: {
+        user_id: userId,
+        challenge_id: challenge_id,
+      },
+      attributes: ["challenge_participation_id"],
+    });
+
+    const participationIds = participations.map(
+      (participation) => participation.challenge_participation_id
+    );
+    res.status(200).json({ participationIds });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to fetch challenge participation IDs" });
+  }
+}
--- src/routes/challengeParticipation.ts
+++ src/routes/challengeParticipation.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { getChallengesByEmail } from "../controllers/ChallengeParticipation";
+import {
+  getChallengesByEmail,
+  getParticipationId,
+} from "../controllers/ChallengeParticipation";
 
 const router = Router();
 
 router.post("/user-challenges", getChallengesByEmail); // POST 요청으로 이메일 전달
+router.post("/participation-id", getParticipationId); // POST 요청으로 이메일과 challenge_id 전달
+
 export default router;
