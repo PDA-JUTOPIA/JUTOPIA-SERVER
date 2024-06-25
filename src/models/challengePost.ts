6cf7861508dc8f2a30cac2842fe6c44f2ca1f044
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-06-25 19:46:20 +0900

    Revert "feat: 챌린지 인증 테이블 내 게시글등록일을 등록 시간을 받아오도록 DB수정"
    This reverts commit fd89f2a0e844b8133f45935ec9e89bd1ee51c0e5.

--- src/models/challengePost.ts
+++ src/models/challengePost.ts
@@ -4,7 +4,6 @@ import {
   Table,
   DataType,
   ForeignKey,
-  Default,
 } from "sequelize-typescript";
 import { ChallengeParticipation } from "./challengeParticipation";
 
@@ -29,7 +28,6 @@ export class ChallengePost extends Model {
   @Column({
     type: DataType.DATE,
     allowNull: false,
-    defaultValue: DataType.NOW, // 기본값을 현재 날짜와 시간으로 설정
   })
   challenge_post_date!: Date;
 
