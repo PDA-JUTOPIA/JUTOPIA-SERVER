fd89f2a0e844b8133f45935ec9e89bd1ee51c0e5
do-yoongyo2 <do.yoongyo2@gmail.com>
2024-06-25 19:37:00 +0900

    feat: 챌린지 인증 테이블 내 게시글등록일을 등록 시간을 받아오도록 DB수정

--- src/models/challengePost.ts
+++ src/models/challengePost.ts
@@ -4,6 +4,7 @@ import {
   Table,
   DataType,
   ForeignKey,
+  Default,
 } from "sequelize-typescript";
 import { ChallengeParticipation } from "./challengeParticipation";
 
@@ -28,6 +29,7 @@ export class ChallengePost extends Model {
   @Column({
     type: DataType.DATE,
     allowNull: false,
+    defaultValue: DataType.NOW, // 기본값을 현재 날짜와 시간으로 설정
   })
   challenge_post_date!: Date;
 
