50b00151a79d535d7e98d16bb1bdb2c3a6464907
이원규 <85729858+OneK-2@users.noreply.github.com>
2024-06-25 20:52:14 +0900

    feat: 주석처리

--- src/routes/challengeParticipation.ts
+++ src/routes/challengeParticipation.ts
@@ -6,7 +6,7 @@ import {
 
 const router = Router();
 
-router.post("/user-challenges", getChallengesByEmail); // POST 요청으로 이메일 전달
+// router.post("/user-challenges", getChallengesByEmail); // POST 요청으로 이메일 전달
 router.post("/join", joinChallenge);
 
 export default router;
