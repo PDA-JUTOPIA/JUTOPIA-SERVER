74c883df5186d425d4b88404616e309d347eaeaf
ymkdev <eveminful@gmail.com>
2024-06-27 16:11:27 +0900

    feat: 유저에 따른 챌린지 전체 참여 횟수 api 구현

--- src/routes/challengeParticipation.ts
+++ src/routes/challengeParticipation.ts
@@ -3,6 +3,7 @@ import {
   getChallengesByEmail,
   joinChallenge,
   checkIsUserInChallenge,
+  getParicipationCountByEmail,
 } from "../controllers/ChallengeParticipation";
 
 const router = Router();
@@ -10,5 +11,6 @@ const router = Router();
 // router.post("/user-challenges", getChallengesByEmail); // POST 요청으로 이메일 전달
 router.post("/join", joinChallenge);
 router.get("/checkJoin/:userId/:challengeId", checkIsUserInChallenge);
+router.get("/get-user-participation-count/:email", getParicipationCountByEmail);
 
 export default router;
